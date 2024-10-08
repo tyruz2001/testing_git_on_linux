49b7b55031f3ea2d7466af03dbdcf8d040edca0c
tyruz2001 <tyruz2001@gmail.com>
2024-10-08 17:59:09 +0800

    first cpp code

---
+++ hello.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Enter some random number: ";
+    int num = 0;
+    cin >> num;
+
+    int numTimesTwo = 0;
+    numTimesTwo = num * 2;
+
+    cout << "The number multiplied by 2 is: " << numTimesTwo;
+}
